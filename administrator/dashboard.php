b7a5e0dc3779ab383f7d7ad15ac46a3b5ab0f576
rlramirez <rlramirez656@gmail.com>
2022-06-14 17:42:17 -0500

    cambiso de la db code seguridad p2

--- administrator/dashboard.php
+++ administrator/dashboard.php
@@ -1,1 +1,15 @@
-<h1>Estoy en el sistema :)</h1>
+<?php
+include("seguridad/seguridad.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bienvenidos al sistema</title>
+</head>
+<body>
+<h1>Hola <?php echo $_SESSION['username'];?></h1>
+<h2><a href="seguridad/exit.php?salir=true">Cerrar sesion</a></h2>
+</body>
+</html>
